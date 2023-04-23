89ed8b264d9bd97ba8bd0492b5566fbe0e53ff25
gakuto-higuchi <gakuto.higuchi@gmail.com>
2023-04-24 03:05:23 +0900

    [add] 現在地ボタンを作成し、色を変更

--- lib/google_map.dart
+++ lib/google_map.dart
@@ -4,6 +4,7 @@ import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'components/bottombar.dart';
@@ -17,46 +18,97 @@ class GoogleMapPage extends StatefulWidget {
 
 class _GoogleMapPageState extends State<GoogleMapPage> {
   final _controller = Completer<GoogleMapController>();
-  final _markers = <Marker>{};
-  LatLng? ontapLatLng;
-  final initiLatLng = const LatLng(34.2925247, 134.0644547);
-  final double maxZoomLevel = 18;
-  final double minZoomLevel = 6;
+  Position? currentPosition;
+  //late GoogleMapController _controller;
+  late StreamSubscription<Position> positionStream;
+  //初期位置
+  final CameraPosition _kGooglePlex = const CameraPosition(
+    target: LatLng(43.0686606, 141.3485613),
+    zoom: 14,
+  );
 
-  late final _initPosition = CameraPosition(target: initiLatLng, zoom: 14.0);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color.fromRGBO(227, 132, 255, 1),
-        body: GoogleMap(
-          initialCameraPosition: _initPosition,
-          markers: _markers,
-          onMapCreated: (GoogleMapController controller) {
-            _onMapCreated(controller);
-          },
-          minMaxZoomPreference:
-              MinMaxZoomPreference(minZoomLevel, maxZoomLevel),
-          myLocationEnabled: true,
-          myLocationButtonEnabled: true,
-        ),
-        bottomNavigationBar: const BottomBar(),
-      ),
-    );
-  }
-
-  Future<void> _onMapCreated(GoogleMapController controller) async {
-    _controller.complete(controller);
-    String value = await DefaultAssetBundle.of(context).loadString(
-        'lib/json/mapstyle_sample.json'); // Jsonファイルをcustom-mapを読み込む
-    GoogleMapController futureController = await _controller.future;
-    futureController.setMapStyle(value); // Controllerを使ってMapをSetする
-  }
+  final LocationSettings locationSettings = const LocationSettings(
+    accuracy: LocationAccuracy.high, //正確性:highはAndroid(0-100m),iOS(10m)
+    distanceFilter: 100,
+  );
 
   @override
   void initState() {
     super.initState();
+
+    //位置情報が許可されていない時に許可をリクエストする
+    Future(() async {
+      LocationPermission permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        await Geolocator.requestPermission();
+      }
+    });
+
+    //現在位置を更新し続ける
+    positionStream =
+        Geolocator.getPositionStream(locationSettings: locationSettings)
+            .listen((Position? position) {
+      currentPosition = position;
+      print(position == null
+          ? 'Unknown'
+          : '${position.latitude.toString()}, ${position.longitude.toString()}');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: _kGooglePlex,
+            myLocationEnabled: true,
+            myLocationButtonEnabled: false, // Disable the default button
+            padding:
+                const EdgeInsets.only(bottom: 60.0), // Add padding to the map
+            onMapCreated: (GoogleMapController controller) {
+              _onMapCreated(controller);
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: FloatingActionButton(
+                backgroundColor: Color.fromRGBO(134, 93, 255, 1),
+                onPressed: _goToCurrentLocation,
+                child: const Icon(Icons.my_location,
+                    size: 35, color: Color.fromRGBO(227, 132, 255, 1)),
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: const BottomBar(),
+    );
+  }
+
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    if (!_controller.isCompleted) {
+      _controller.complete(controller);
+      String value = await DefaultAssetBundle.of(context).loadString(
+          'lib/json/mapstyle_sample.json'); // Jsonファイルをcustom-mapを読み込む
+      GoogleMapController futureController = await _controller.future;
+      futureController.setMapStyle(value); // Controllerを使ってMapをSetする
+    }
+  }
+
+  Future<void> _goToCurrentLocation() async {
+    if (currentPosition != null) {
+      final GoogleMapController controller = await _controller.future;
+      controller.animateCamera(CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(currentPosition!.latitude, currentPosition!.longitude),
+          zoom: 14,
+        ),
+      ));
+    }
   }
 }
 //gakuto.higuchi@gmail.com
