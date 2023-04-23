9540e36ac95213402b077da55a0bb9e3dca10fc0
gakuto-higuchi <gakutohiguchi@gakutohiguchis-MacBook-Pro.local>
2023-04-23 16:20:38 +0900

    [add] bottomNavを作成

---
+++ lib/components/bottombar.dart
@@ -0,0 +1,42 @@
+﻿import 'package:flutter/material.dart';
+
+class BottomBar extends StatelessWidget {
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      color: Color.fromRGBO(134, 93, 255, 1),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.home, size: 35),
+            color: Color.fromRGBO(227, 132, 255, 1),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search, size: 35),
+            color: Color.fromRGBO(227, 132, 255, 1),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add_box, size: 35),
+            color: Color.fromRGBO(227, 132, 255, 1),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.favorite, size: 35),
+            color: Color.fromRGBO(227, 132, 255, 1),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.person_outline, size: 35),
+            color: Color.fromRGBO(227, 132, 255, 1),
+          ),
+        ],
+      ),
+    );
+  }
+}
